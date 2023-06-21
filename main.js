13e773c1e13a493715c091259a1d0c771475e2f2
garder500 <65552225+garder500@users.noreply.github.com>
2023-06-21 10:52:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const { Headers } = require("node-fetch");
  * @returns {Promise<Response>} 
  */
 async function fetch(url){ 
-  let response = request(url, {
+  let response = await request(url, {
     headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
